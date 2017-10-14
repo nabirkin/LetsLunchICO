33c1b6db3d4af81e35777b85d9bbbc1a2554806b
Artem.Nabirkin <artem.nabirkin@gmail.com>
2017-10-13 20:31:36 -0400

    add sample contract

---
+++ contracts/MyContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.4;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract MyContract is Ownable {
+  function MyContract() {
+    // constructor
+  }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,2 +1,5 @@
+var MyContract = artifacts.require("./MyContract.sol");
+
 module.exports = function(deployer) {
+    deployer.deploy(MyContract);
 };
